0c1d139afadcccb097192c43f13dc94ab7e00986
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-17 12:03:40 +0530

    Initialize an express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+
+// Create a new express application instance
+const app = express();
+
+dotenv.config();
+
+// Root path
+app.get("/", (_req, res) => {
+  res.send("Hello world!");
+});
+
+// Set up the port
+const port = process.env.PORT || 3000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
