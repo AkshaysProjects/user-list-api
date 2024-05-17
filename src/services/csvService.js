4f78235b47a7f317048f6599c855891c64c8d130
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-05-17 12:20:05 +0530

    Create a service to parse CSV and allocate fallback from list

---
+++ src/services/csvService.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+import csv from "csv-parser";
+
+const parseCSV = (filePath, list) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        const properties = {};
+        for (let prop of list.customProperties) {
+          properties[prop.title] = data[prop.title] || prop.fallback;
+        }
+        results.push({ name: data.name, email: data.email, properties });
+      })
+      .on("end", () => {
+        resolve(results);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+};
+
+export default parseCSV;
